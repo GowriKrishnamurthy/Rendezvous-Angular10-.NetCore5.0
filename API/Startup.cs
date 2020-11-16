7f150bc84b3a9baeb311aefa31478f30b1fa63bc
GowriKrishnamurthy <35420783+GowriKrishnamurthy@users.noreply.github.com>
2020-11-16 16:54:57 +1100

    Added a new token service, using JSON Web Tokens

--- API/Startup.cs
+++ API/Startup.cs
@@ -23,11 +23,18 @@ namespace API
             Configuration = configuration;
         }
 
+        public Startup(IConfiguration configuration) 
+        {
+            this.Configuration = configuration;
+   
+        }
+       
         public IConfiguration Configuration { get; }
 
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddScoped<ITokenService,TokenService>();
             services.AddDbContext<DataContext>(option => 
             {
                 option.UseSqlite(Configuration.GetConnectionString("DefaultConnection"));
